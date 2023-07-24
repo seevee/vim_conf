df606021e02cc289feffa7bdbc7001304456fe8e
Chris Vincent <chris@cvbytes.com>
2023-07-24 10:14:28 -0600

    refactor: removed vim entirely

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-# determine OS and run appropriate dependency installation script
+# install and configure neovim
 
-VIM_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-source $VIM_DIR/lib/bash-utils/index.sh
+REPO_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+source $REPO_DIR/lib/bash-utils/index.sh
 
-h1 "vim/nvim dotfiles installation"
+h1 "neovim dotfiles installation"
 
 h1_emphasis "found OS: " "$OSTYPE"
 h1_emphasis "found distro: " $(get_linux_distribution)
@@ -14,31 +14,17 @@ install_packages cmake git neovim
 success "done"
 
 h1b "sylminking configuration..."
-overwrite_symlink $VIM_DIR $HOME/.vim
-overwrite_symlink $VIM_DIR/vimrc $HOME/.vimrc
 mkdir -p $HOME/.config
-overwrite_symlink $VIM_DIR/nvim $HOME/.config/nvim
+overwrite_symlink $REPO_DIR/nvim $HOME/.config/nvim
 success "done"
 
-# install vim-plug and packages
-if command -v vim &> /dev/null; then
-  h1b "installing/updating vim-plug and packages..."
-
-  vim +PlugUpgrade +PlugInstall +PlugUpdate +CocUpdate +qall
-
-  success "done"
-else
-  h2 "vim not found"
-fi
 # install Lazy.nvim and sync packages
 if command -v nvim &> /dev/null; then
   h1b "installing/updating lazy.nvim and packages..."
-
   nvim --headless "+Lazy! sync" +qa
-
   success "done"
 else
-  h2 "nvim not found"
+  err "nvim not found"
 fi
 
-h1_success "vim/neovim installation complete"
+h1_success "neovim installation complete"
