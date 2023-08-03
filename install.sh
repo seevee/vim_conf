5f4df452d01d756b1c2e9fd681d319d3861ae633
Chris Vincent <chris@cvbytes.com>
2023-08-03 12:02:16 -0600

    lint: added shellcheck config, fixed lint warnings

--- install.sh
+++ install.sh
@@ -2,20 +2,20 @@
 # install and configure neovim
 
 REPO_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-source $REPO_DIR/lib/bash-utils/index.sh
+source "$REPO_DIR"/lib/bash-utils/index.sh
 
 h1 "neovim dotfiles installation"
 
 h1_emphasis "found OS: " "$OSTYPE"
-h1_emphasis "found distro: " $(get_linux_distribution)
+h1_emphasis "found distro: " "$(get_linux_distribution)"
 
 h1b "installing dependencies..."
 install_packages cmake git neovim
 success "done"
 
 h1b "symlinking configuration..."
-mkdir -p $HOME/.config
-overwrite_symlink $REPO_DIR/nvim $HOME/.config/nvim
+mkdir -p "$HOME"/.config
+overwrite_symlink "$REPO_DIR"/nvim "$HOME"/.config/nvim
 success "done"
 
 # install Lazy.nvim and sync packages
