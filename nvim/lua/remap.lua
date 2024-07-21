9bd3bb01a5dae284117bb6e1a56c5bb9d7b6991d
Chris Vincent <chris@cvbytes.com>
2024-07-21 11:11:40 -0600

    add godot, love2d support

--- nvim/lua/remap.lua
+++ nvim/lua/remap.lua
@@ -1,4 +1,4 @@
-vim.g.mapleader = ' '
+vim.g.mapleader = " "
 
 -- move visually highlighted code up/down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -17,32 +17,32 @@ vim.keymap.set("n", "N", "Nzzzv")
 
 -- greatest remap ever
 -- paste without overwriting the paste buffer
-vim.keymap.set('x', '<leader>p', [["_dP]])
+vim.keymap.set("x", "<leader>p", [["_dP]])
 
 -- next greatest remap ever : asbjornHaland
 -- shorthand for yanking to system clipboard
-vim.keymap.set({'n', 'v'}, '<leader>y', [["+y]])
-vim.keymap.set('n', '<leader>Y', [["+Y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 -- delete to void register
-vim.keymap.set({'n', 'v'}, '<leader>d', [["_d]])
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 -- 'Q' is evil
-vim.keymap.set('n', 'Q', '<nop>')
+vim.keymap.set("n", "Q", "<nop>")
 
 -- cancel ThePrimeagen for this one, don't @ me
-vim.keymap.set('i', '<C-c>', '<Esc>')
+vim.keymap.set("i", "<C-c>", "<Esc>")
 
 -- quickfix navigation
-vim.keymap.set('n', '<C-k>', '<cmd>cnext<CR>zz')
-vim.keymap.set('n', '<C-j>', '<cmd>cprev<CR>zz')
-vim.keymap.set('n', '<leader>k', '<cmd>lnext<CR>zz')
-vim.keymap.set('n', '<leader>j', '<cmd>lprev<CR>zz')
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 -- find/replace current word
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 -- source the file
-vim.keymap.set('n', '<leader><leader>', function()
-    vim.cmd('source')
+vim.keymap.set("n", "<leader><leader>", function()
+	vim.cmd("source")
 end)
